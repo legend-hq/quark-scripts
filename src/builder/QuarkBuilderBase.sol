c979bb01dddd166d159180045f31e74c17f8137f
Hans Wang <2709448+cwang25@users.noreply.github.com>
2024-11-22 12:18:25 -0800

    add if to skip if either borrow/repay is having 0 amount (#8)
    * add if to skip if either borrow/repay is having 0 amount
    
    * bump version to 0.3.1

--- src/builder/QuarkBuilderBase.sol
+++ src/builder/QuarkBuilderBase.sol
@@ -18,7 +18,7 @@ import {QuarkOperationHelper} from "src/builder/QuarkOperationHelper.sol";
 import {List} from "src/builder/List.sol";
 import {HashMap} from "src/builder/HashMap.sol";
 
-string constant QUARK_BUILDER_VERSION = "0.3.0";
+string constant QUARK_BUILDER_VERSION = "0.3.1";
 
 contract QuarkBuilderBase {
     /* ===== Output Types ===== */
